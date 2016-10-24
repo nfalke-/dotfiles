b587ea2502c96729f82f98f0efb29323cf7f0724
nate <nate.falke@gmail.com>
2016-10-24 18:34:50 -0500

    lots of changes

--- .config/pianobar/event_command.py
+++ .config/pianobar/event_command.py
@@ -10,6 +10,7 @@ fn = join(path, 'pianobar', 'nowplaying')
 info = sys.stdin.readlines()
 cmd = sys.argv[1]
 
+
 if cmd == 'songstart':
     with open(fn, 'w') as f:
         f.write(info[1].split('=')[1].strip() + " by " + info[0].split('=')[1].strip())
